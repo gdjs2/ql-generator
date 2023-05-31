e4c218503fd3e831127cb9e46a896355f2991641
gdjs2 <xzqgdjs2@icloud.com>
2023-05-30 22:44:44 -0700

    add:
    library for http request
    
    some fields in CodeQLEngine

--- src/engine.rs
+++ src/engine.rs
@@ -1,13 +1,28 @@
+use ureq::Agent;
+
 pub trait Engine {
 	fn is_alloactor(&self, _func: &str) -> bool {
 		return true;
 	}
 }
 
-pub struct DefaultEngine {
+pub struct CodeQLEngine {
+	api_token: String,
+	agent: Agent
+}
+
+impl CodeQLEngine {
+
+	pub fn new(t: String) -> Self {
+		CodeQLEngine { 
+			api_token: t,
+			agent: ureq::AgentBuilder::new().build()
+		}
+	}
+
 
 }
 
-impl Engine for DefaultEngine {
-	
+impl Engine for CodeQLEngine {
+
 }
