6b97f6a18a2455b0652b0033ae5f44b4c49f73ee
gdjs2 <zhaoqi.xiao@gdjs2.cn>
2023-06-01 15:57:53 -0700

    tmp commit

--- src/generator.rs
+++ src/generator.rs
@@ -1,10 +1,44 @@
-use std::path::PathBuf;
+use std::{path::{PathBuf, Path}, fs};
+
+use fs_extra::dir::{CopyOptions, copy};
 
 pub trait Generator {
-	fn gen() -> PathBuf;
+	fn gen(&self, p: &Path);
 }
 
 pub struct CodeQLGenerator {
-	ql: PathBuf
+	ql_dir: PathBuf,
+	replace_pts: Vec<PathBuf>
 }
 
+impl CodeQLGenerator {
+	pub fn new(ql_dir_s: &str, replace_pts_s: Vec<&str>) -> Self {
+		let ql_dir = PathBuf::new().join(ql_dir_s);
+		let mut replace_pts = Vec::new();
+		for s in replace_pts_s {
+			replace_pts.push(PathBuf::new().join(s));
+		}
+		CodeQLGenerator { ql_dir, replace_pts }
+	}
+}
+
+impl Generator for CodeQLGenerator {
+	fn gen(&self, p: &Path) {
+		
+		log::debug!("[CodeQL Generator][gen()] Copy QL directory from {{{:?}}} to {{{:?}}}", self.ql_dir, p);
+		let ops = CopyOptions::new();
+		let res = copy(&self.ql_dir, p, &ops);
+		log::debug!("[CodeQL Generator][gen()] Copy Result {{{:?}}}", res);
+
+		let tar_dir = PathBuf::new().join(p).join(self.ql_dir.file_name().unwrap());
+		log::debug!("[CodeQL Generator][gen()] The target directory is \"{:?}\"", tar_dir);
+
+		log::debug!("[CodeQL Generator][gen()] Start generating qls...");
+		for pt in &self.replace_pts {
+			let tar_file = PathBuf::new().join(&tar_dir).join(pt);
+			log::debug!("[CodeQL Generator][gen()] Generating {{{}}}<{}>");
+		}
+
+
+	}
+}
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,6 +1,6 @@
-use std::{str::FromStr, env};
+use std::{str::FromStr, env, path::Path};
 
-use ql_generator::{extractor::{CodeQLExtractor, Extractor}, engine::{ChatGPTEngine, Engine}};
+use ql_generator::{extractor::{CodeQLExtractor, Extractor}, engine::{ChatGPTEngine, Engine}, generator::{self, CodeQLGenerator, Generator}};
 
 #[test]
 fn test_codeql_extractor_and_engine() {
@@ -16,5 +16,17 @@ fn test_codeql_extractor_and_engine() {
 		println!("{}", f);
 		log::debug!("[test_codeql_extractor_and_engine]: {}", engine.is_allocator(&f));
 	}
+
+}
+
+#[test]
+fn test_codeql_generator() {
+
+	env_logger::init();
+
+	let pts_s = vec!["./allocator.ql"];
+	let generator = CodeQLGenerator::new("/home/gdjs2/Desktop/codeql-project/ql-generator/ql-generator-qls", pts_s);
+
+	generator.gen(Path::new("./tmp/.work_dir"));
 
 }
